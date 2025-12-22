b0db87d4ddfff8ef1c631e2fb88fb2e54e69ffd5
DuhDiamond <duhdiamond76@gmail.com>
2025-12-21 16:51:44 -0800

    Git pages first commit test

---
+++ src/FrontPage.jsx
@@ -0,0 +1,9 @@
+const FrontPage = () => {
+  return (
+    <>
+      <h1>Testing</h1>
+    </>
+  )
+}
+
+export default FrontPage
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { strictMode } from 'react'
+import FrontPage from './FrontPage'
+// import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <FrontPage />
+)
